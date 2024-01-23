604ce394b9eb64c9dd8183b7e84874d2b322babe
DonDuck1 <malj0042@hz.nl>
2024-01-23 14:00:58 +0100

    Renamed some things and this should be the final release

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,13 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
+echo "$1"
 
 echo "Building frontend has begun"
-ls
 apk add --update npm
 # npm ci
 npm install
 npm run build --if-present
 echo "Building frontend has finished"
 
+time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
-
-#   steps:
-#     - run: echo "Building frontend has begun"
-#     - uses: actions/checkout@v4
-#     - run: npm ci
-#     - run: npm run build --if-present
-#     - run: echo "Building frontend has finished"
