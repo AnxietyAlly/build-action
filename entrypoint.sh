3a3eda4328948933ebfe588e919933430c7c7591
DonDuck1 <malj0042@hz.nl>
2024-01-23 13:02:08 +0100

    Install npm in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,10 @@
 echo "Hello $1"
 time=$(date)
 echo "Building frontend has begun"
+apk add --update npm
 npm ci
 npm run build --if-present
-"Building frontend has finished"
+echo "Building frontend has finished"
 
 echo "time=$time" >> $GITHUB_OUTPUT
 
