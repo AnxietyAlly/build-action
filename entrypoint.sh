3077875960f559b6e601cc550a8786fd01140b57
DonDuck1 <malj0042@hz.nl>
2024-01-23 12:58:40 +0100

    Build action steps in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,16 @@
 
 echo "Hello $1"
 time=$(date)
+echo "Building frontend has begun"
+npm ci
+npm run build --if-present
+"Building frontend has finished"
+
 echo "time=$time" >> $GITHUB_OUTPUT
+
+#   steps:
+#     - run: echo "Building frontend has begun"
+#     - uses: actions/checkout@v4
+#     - run: npm ci
+#     - run: npm run build --if-present
+#     - run: echo "Building frontend has finished"
